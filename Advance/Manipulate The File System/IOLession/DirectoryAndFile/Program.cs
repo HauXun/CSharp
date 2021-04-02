f04a87f0417fa9b5fc751b699bee6404e2e77565
HauXun <hautrieu65@gmail.com>
2021-04-02 20:12:50 +0700

    Init Commit 🚀🚀🚀

---
+++ Advance/Manipulate The File System/IOLession/DirectoryAndFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace DirectoryAndFile
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DirectoryInfo myDir = new DirectoryInfo(@"E:\Download");
+			if (myDir.Exists)
+			{
+				FileInfo[] files = myDir.GetFiles();
+				DirectoryInfo[] dfiles = myDir.GetDirectories();
+				//foreach (FileInfo file in files)
+				//{
+				//	WriteLine(file.FullName);
+				//}
+				WriteLine("\n");
+				foreach (DirectoryInfo file in dfiles)
+				{
+					WriteLine(file.FullName);
+				}
+			}
+			else
+			{
+				WriteLine("Directory is not existed");
+			}
+		}
+	}
+}
