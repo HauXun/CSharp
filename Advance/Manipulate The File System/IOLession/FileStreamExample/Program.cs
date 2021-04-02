838be907365a4a172c157b509bfb457f809c3b00
HauXun <hautrieu65@gmail.com>
2021-04-02 17:34:52 +0700

    Init Commit 🚀🚀🚀

---
+++ Advance/Manipulate The File System/IOLession/FileStreamExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace FileStreamExample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			FileStream fileStream = new FileStream(@"text.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+			for (int i = 1; i <= 20; i++)
+			{
+				fileStream.WriteByte((byte)i);
+			}
+			fileStream.Position = 0;
+			WriteLine("Write OK");
+			for (int i = 0; i <= 20; i++)
+			{
+				Write(fileStream.ReadByte() + "\t");
+			}
+			fileStream.Close();
+		}
+	}
+}
