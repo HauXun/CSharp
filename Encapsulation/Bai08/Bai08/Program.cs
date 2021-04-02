c7e2d1103ca48732dc0e2837cb2cb3607558056d
HauXun <hautrieu65@gmail.com>
2021-04-02 10:58:24 +0700

    Init Commit 🚀🚀🚀

--- Encapsulation/Bai08/Bai08/Program.cs
+++ Encapsulation/Bai08/Bai08/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace Bai08
 {
@@ -6,7 +7,62 @@ namespace Bai08
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Account acc = new Account(001, "Tuan", 2000);
+			acc.display();
+			acc.withdraw(2100);
+			acc.deposit(600);
+			WriteLine("Balance: " + acc.getBalance());
+			acc.withdraw(2100);
+			WriteLine("Balance: " + acc.getBalance());
+		}
+	}
+
+	class Account
+	{
+		int id;
+		string name;
+		int balance = 0;
+
+		public Account(int id, string name)
+		{
+			this.id = id;
+			this.name = name;
+		}
+
+		public Account(int id, string name, int balance)
+		{
+			this.id = id;
+			this.name = name;
+			this.balance = balance;
+		}
+
+		public void setName(string name)
+		{
+			this.name = name;
+		}
+
+		public int getID() => id;
+		public string getName() => name;
+		public int getBalance() => balance;
+
+		public void deposit(int amount)
+		{
+			balance += amount;
+		}
+
+		public void withdraw(int amount)
+		{
+			if (balance >= amount)
+				balance -= amount;
+			else
+				WriteLine("That amount exceeds your current balance.");
+		}
+
+		public void display()
+		{
+			WriteLine("ID: " + id);
+			WriteLine("Name: " + name);
+			WriteLine("Balamce: " + balance);
 		}
 	}
 }
