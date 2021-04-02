b93f8c403afdbbfc48e8f4758428d00d12167bcd
HauXun <hautrieu65@gmail.com>
2021-04-02 09:09:15 +0700

    Init Commit 🚀🚀🚀

--- Encapsulation/Bai04/Bai04/Program.cs
+++ Encapsulation/Bai04/Bai04/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace Bai04
 {
@@ -6,7 +7,63 @@ namespace Bai04
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Employee e = new Employee(1, "Thuy", "Vy", 1000);
+			WriteLine("ID: " + e.getID());
+			WriteLine("Name: " + e.getFullName());
+			WriteLine("Salary: " + e.getSalary());
+
+			WriteLine("\n");
+
+			e.setID(2);
+			e.setFirstName("Hoai");
+			e.setLastName("Ngoc");
+			e.setSalary(2000);
+			WriteLine("ID: " + e.getID());
+			WriteLine("Name: " + e.getFullName());
+			WriteLine("Salary: " + e.getSalary());
 		}
+	}
+
+	class Employee
+	{
+		int id;
+		string firstName;
+		string lastName;
+		int salary;
+
+		public Employee(int id, string firstName, string lastName, int salary)
+		{
+			this.id = id;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.salary = salary;
+		}
+
+		public void setID(int id)
+		{
+			this.id = id;
+		}
+
+		public void setFirstName(string firstName)
+		{
+			this.firstName = firstName;
+		}
+
+		public void setLastName(string lastName)
+		{
+			this.lastName = lastName;
+		}
+
+		public void setSalary(int salary)
+		{
+			this.salary = salary;
+		}
+
+		public int getID() => id;
+		public string getFirstName() => firstName;
+		public string getLastName() => lastName;
+		public int getSalary() => salary;
+
+		public string getFullName() => firstName + " " + lastName;
 	}
 }
