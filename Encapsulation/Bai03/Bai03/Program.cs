ad0c57e285980878a7a968855dcbaedc73bfdd69
HauXun <hautrieu65@gmail.com>
2021-04-02 08:51:17 +0700

    Init Commit 🚀🚀🚀

--- Encapsulation/Bai03/Bai03/Program.cs
+++ Encapsulation/Bai03/Bai03/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace Bai03
 {
@@ -6,7 +7,79 @@ namespace Bai03
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Student s = new Student(1001, "Trung", 24, "Ha Noi", 5);
+			WriteLine("ID: " + s.getID());
+			WriteLine("Name: " + s.getName());
+			WriteLine("Age: " + s.getAge());
+			WriteLine("Address: " + s.getAddress());
+			WriteLine("Score: " + s.getScore());
+
+			WriteLine("\n");
+
+			s.setScore(-7);
+			WriteLine("Score: " + s.getScore());
+			s.setScore(15);
+			WriteLine("Score: " + s.getScore());
 		}
+	}
+
+	class Student
+	{
+		int id;
+		string name;
+		int age;
+		string address;
+		int score;
+
+		public Student()
+		{
+
+		}
+
+		public Student(int id, string name, int age, string address, int score)
+		{
+			this.id = id;
+			this.name = name;
+			this.age = age;
+			this.address = address;
+			this.setScore(score);
+		}
+
+		public void setID(int id)
+		{
+			this.id = id;
+		}
+
+		public void setName(string name)
+		{
+			this.name = name;
+		}
+
+		public void setAge(int age)
+		{
+			this.age = age;
+		}
+
+		public void setAddress(string address)
+		{
+			this.address = address;
+		}
+
+		public void setScore(int score)
+		{
+			if (score < 0)
+				this.score = 0;
+			else if (score > 10)
+				this.score = 10;
+			else
+				this.score = score;
+		}
+
+
+		public int getID() => id;
+		public string getName() => name;
+		public int getAge() => age;
+		public string getAddress() => address;
+		public int getScore() => score;
 	}
 }
---
+++ Encapsulation/Bai04/Bai04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bai04
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
