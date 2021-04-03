2837e1dfb969274cf821b130586684ee8a392e3b
HauXun <hautrieu65@gmail.com>
2021-04-03 09:26:15 +0700

    Init Commit 🚀🚀🚀

--- Inheritance/Exercise 2/Exercise 2/Program.cs
+++ Inheritance/Exercise 2/Exercise 2/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using static System.Console;
 
 namespace Exercise_2
 {
+	class Shape
+	{
+		protected int width;
+		protected int height;
+
+		public void setWidth(int width)
+		{
+			this.width = width;
+		}
+
+		public void setHeight(int height)
+		{
+			this.height = height;
+		}
+		
+	}
+	class Rectangle : Shape
+	{
+		public int GetArea() => width * height;
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Rectangle rec = new Rectangle();
+			rec.setWidth(12);
+			rec.setHeight(30);
+			WriteLine("Area: {0}", rec.GetArea());
 		}
 	}
 }
---
+++ Inheritance/Exercise 2/InitialBaseClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InitialBaseClass
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
