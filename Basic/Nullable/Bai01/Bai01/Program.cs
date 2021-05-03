8d50bbc74cee63e086da5bc4291880814840b809
HauXun <hautrieu65@gmail.com>
2021-05-03 16:33:33 +0700

    Init Commit 🚀🚀🚀

---
+++ Basic/Nullable/Bai01/Bai01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace Bai01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int? age = null;
+			float? mark = null;
+			Nullable<double> price = null;
+			WriteLine("Age: " + age);
+			WriteLine("Mark: " + (mark.HasValue ? mark.Value : 0));
+			WriteLine("Price: " + (price ?? 0));
+		}
+	}
+}
