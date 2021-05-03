769a902db09dcf9412f5683481e84a09e0ace801
HauXun <hautrieu65@gmail.com>
2021-05-03 17:17:35 +0700

    Init Commit 🚀🚀🚀

---
+++ Advance/Namespace/Bai01/Bai01/Program.cs
@@ -0,0 +1,42 @@
+﻿using static System.Console;
+
+namespace Bai01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			First.A a = new First.A();
+			a.Display();
+			Third.Second.A a2 = new Third.Second.A();
+			a2.Display();
+			ReadKey();
+		}
+	}
+}
+
+namespace First
+{
+	class A
+	{
+		public void Display()
+		{
+			WriteLine("This is a class A in First");
+		}
+	}
+}
+
+namespace Third
+{
+	namespace Second
+	{
+		class A
+		{
+			public void Display()
+			{
+				WriteLine("This is a class A in Second");
+			}
+		}
+	}
+
+}
