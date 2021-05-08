e95062d68d83e238ec5a2d29af39aed952a894ef
HauXun <hautrieu65@gmail.com>
2021-05-08 09:49:18 +0700

    Additional

---
+++ Advance/Manipulate The File System/Bai03/Bai03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Bai03
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Path.DirectorySeparatorChar);
+			var path = Path.Combine("TranDan", "SonTung", "JackCam", "TranDucBo.exe");
+			//path = Path.ChangeExtension(path, "dll");
+			Console.WriteLine(path);
+			Console.WriteLine(Path.GetFileName(path));
+			Console.WriteLine(Path.GetExtension(path));
+			Console.WriteLine(Path.GetFullPath(path));
+			Console.WriteLine(Path.GetDirectoryName(path));
+
+			path = Path.GetRandomFileName();
+			Console.WriteLine(path);
+			path = Path.GetTempFileName();
+			Console.WriteLine(path);
+		}
+	}
+}
