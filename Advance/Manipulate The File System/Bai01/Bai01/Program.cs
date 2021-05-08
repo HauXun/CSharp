12161565b6b7f58f1edfc39765467c1a9a30c22e
HauXun <hautrieu65@gmail.com>
2021-05-08 09:03:10 +0700

    Additional

---
+++ Advance/Manipulate The File System/Bai01/Bai01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace Bai01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var drives = DriveInfo.GetDrives();
+			foreach (var driveInfo in drives)
+			{
+				WriteLine("Drive: {0]", driveInfo.Name);
+				WriteLine("Drive type {0}", driveInfo.DriveType);
+				WriteLine("Volume label: {0}", driveInfo.VolumeLabel);
+				WriteLine("File system: {0}", driveInfo.DriveFormat);
+				WriteLine("Available space to current user:{0, 15} bytes", driveInfo.AvailableFreeSpace);
+				WriteLine("Total available space:          {0, 15} bytes", driveInfo.TotalFreeSpace);
+				WriteLine("Total size of drive:            {0, 15} bytes ", driveInfo.TotalSize);
+				WriteLine("".PadLeft(40, '='));
+			}
+		}
+	}
+}
