b796d2d901260fd33b1e8ea696664be466cfc3d5
HauXun <hautrieu65@gmail.com>
2021-05-08 09:03:32 +0700

    Additional

---
+++ Basic/Nullable/Bai02/Bai02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Bai02
+{
+	class ABC
+	{
+		public void XinChao() => Console.WriteLine("Xin chao Tran Dan");
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ABC a = null;
+			if (a != null)
+				a.XinChao();
+			a?.XinChao();
+
+			int? age = null;
+			age = 10;
+			if (age.HasValue || age != null)
+			{
+				int _age = age.Value;
+				_age = (int)age;
+				Console.WriteLine(_age);
+			}
+		}
+	}
+}
