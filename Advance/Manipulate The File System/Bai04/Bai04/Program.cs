1f560bcc7bb8e55b42211297dd8cee5e0a5ca687
HauXun <hautrieu65@gmail.com>
2021-05-08 10:01:28 +0700

    Additional

---
+++ Advance/Manipulate The File System/Bai04/Bai04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Bai04
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string filename = "text.txt";
+			string content = "Xin chao cac ban 2020";
+			//File.WriteAllText(filename, content);
+			//File.AppendAllText(filename, content);
+			content = File.ReadAllText(filename);
+			Console.WriteLine(content);
+			//File.Move("text.txt", "TranDan.txt");
+			//File.Copy("text.txt", "SonTung.txt");
+			File.Delete("SonTung.txt");
+		}
+	}
+}
