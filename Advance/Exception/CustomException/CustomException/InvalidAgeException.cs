ab7ad9acbcbdfefec509d31dd0f3b0e099259a04
HauXun <hautrieu65@gmail.com>
2021-05-24 13:15:41 +0700

    Additional Ex

---
+++ Advance/Exception/CustomException/CustomException/InvalidAgeException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CustomException
+{
+	class InvalidAgeException : Exception
+	{
+		public InvalidAgeException(string message) : base (message) { }
+		public InvalidAgeException(string message, Exception innerException) :
+			base(message, innerException)
+		{
+
+		}
+	}
+}
---
+++ Advance/Exception/CustomException/CustomException/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace CustomException
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			WriteLine("Please enter your age >> ");
+			string input = ReadLine();
+
+			try
+			{
+				byte age = byte.Parse(input);
+				if (age < 18)
+					throw new InvalidAgeException("You didn't enough age");
+			}
+			catch (FormatException fe)
+			{
+				WriteLine("You have to type a number");
+			}
+			catch(InvalidAgeException iae)
+			{
+				WriteLine(iae.Message);
+			}
+		}
+	}
+}
