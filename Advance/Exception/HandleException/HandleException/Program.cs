d26d22cde56dfae2388334a0b62d5e79872e82dc
HauXun <hautrieu65@gmail.com>
2021-05-24 11:50:44 +0700

    Init Commit 🚀🚀🚀

---
+++ Advance/Exception/HandleException/HandleException/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using static System.Console;
+
+namespace HandleException
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int num1 = 0;
+			int num2 = 0;
+			int divNum;
+			try
+			{
+				WriteLine("Please enter first number >> ");
+				num1 = int.Parse(ReadLine());
+			}
+			catch (FormatException fe)
+			{
+				WriteLine(fe.Message);
+				WriteLine(fe.Source);
+				WriteLine("Invalid format");
+			}
+			catch(Exception e)
+			{
+				WriteLine("Has an error. Please try again!");
+			}
+			finally
+			{
+				WriteLine("Finally");
+			}
+
+			try
+			{
+				WriteLine("Please enter second number >> ");
+				num2 = int.Parse(ReadLine());
+			}
+			catch (FormatException fe)
+			{
+				WriteLine(fe.Message);
+				WriteLine(fe.Source);
+				WriteLine("Invalid format");
+			}
+			catch (Exception e)
+			{
+				WriteLine("Has an error. Please try again!");
+			}
+
+			try
+			{
+				divNum = num1 / num2;
+				WriteLine("Result of num1 / num2 = {0}", divNum);
+			}
+			catch(DivideByZeroException dbe)
+			{
+				WriteLine(dbe.Message);
+				WriteLine(dbe.Source);
+			}
+			catch (Exception e)
+			{
+				WriteLine("Has an error. Please try again!");
+			}
+		}
+	}
+}
