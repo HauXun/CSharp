4bc19ebadcca48c31898a9d5c7241f570620d690
HauXun <hautrieu65@gmail.com>
2021-05-24 19:12:02 +0700

    Additonal Ex

---
+++ Advance/Properties/Exercise 3/Exercise 3/BankAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+	class BankAccount
+	{
+		public long AccountNumber { get; }  // ReadOnly
+		public string Owner { get; set; }   // Can Read, Write value
+		public decimal Balance { get; }     // Can only read
+
+		// Gửi tiền
+		public void Deposit(decimal amount, DateTime date, string note)
+		{
+			// Money sent to bank account
+
+		}
+
+		public void Withdraw(decimal amount, DateTime date, string note)
+		{
+			// Withdraw money to buy something
+
+		}
+
+		private long GeneratorAccountNumbers() => ((DateTimeOffset)DateTime.Now).ToUnixTimeMilliseconds();
+		public BankAccount(string owner, decimal initialBalance)
+		{
+			Owner = owner;
+			AccountNumber = GeneratorAccountNumbers();
+			Balance = initialBalance;
+			Console.WriteLine("Object initialized with \n" +
+				$"Account number = {AccountNumber}\n" +
+				$"Owner = {Owner}\n" +
+				$"Balance = {Balance}\n");
+		}
+	}
+}
---
+++ Advance/Properties/Exercise 3/Exercise 3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise_3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var account = new BankAccount("Tran Dan", 1200);
+			WriteLine("Account has been created");
+		}
+	}
+}
