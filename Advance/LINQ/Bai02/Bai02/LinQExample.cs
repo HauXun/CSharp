ad7eadfa6c170f828acc4e1d9857068952fa94e8
HauXun <hautrieu65@gmail.com>
2021-05-25 18:11:05 +0700

    Additonal Ex

---
+++ Advance/LINQ/Bai02/Bai02/LinQExample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+
+namespace Bai02
+{
+	class LinQExample
+	{
+		public static void queryIntNumber()
+		{
+			// 1. Data Source
+			int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+			// 2. Query Creation
+			var queryResult = from number in numbers
+							  where (number % 2) == 0
+							  select number;
+
+			// 3. Query Execution
+			foreach (var number in queryResult)	// IEnumrable<int>
+			{
+				Write($"{number} ");
+			}
+
+			WriteLine("\nConvert query's result to array");
+			var arrayOfNumber = queryResult.ToArray();
+			foreach (var number in arrayOfNumber) // This is a array
+			{
+				Write($"{number} ");
+			}
+
+			WriteLine("\nConvert query's result to list");
+			var listOfNumber = queryResult.ToArray();
+			foreach (var number in listOfNumber) // This is a list
+			{
+				Write($"{number} ");
+			}
+			WriteLine("\nEnd of conversion !");
+		}
+	}
+}
---
+++ Advance/LINQ/Bai02/Bai02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bai02
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			LinQExample.queryIntNumber();
+		}
+	}
+}
