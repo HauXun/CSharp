3daf38ec731cee44a914f54a664d3f5c3112a15e
HauXun <hautrieu65@gmail.com>
2021-05-25 18:49:17 +0700

    Additonal Ex

---
+++ Advance/LINQ/Bai03/Bai03/LinQExample.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using static System.Console;
+
+namespace Bai03
+{
+	class LinQExample
+	{
+		public static void queryXMLFile(string fileName)
+		{
+			// 1. Data Source
+			var xmlDocument = XDocument.Load(@fileName);
+
+			// 2. Query Creation
+			IEnumerable<string> filmNames = from film in xmlDocument.Descendants("film")
+											//where (int)film.Element("releaseYear") == 1977
+											//where (int)film.Element("releaseYear") > 1900
+											select film.Element("name").Value;
+			// Query result => object => contain name, releaseYear => how to do this ?
+			var foundedFilms = from film in xmlDocument.Descendants("film")
+							   //where (int)film.Element("releaseYear") > 1900
+							   select new
+							   {
+								   Name = film.Element("name").Value,
+								   ReleaseYear = film.Element("releaseYear").Value
+							   };
+
+			// 3. Query Execution
+			foreach (var filmName in filmNames)
+			{
+				WriteLine($"{filmName} ");
+			}
+
+			WriteLine('\n');
+
+			foreach (var foundedFilm in foundedFilms)
+			{
+				WriteLine($"Name: {foundedFilm.Name}, ReleaseYear: {foundedFilm.ReleaseYear}");
+			}
+		}
+	}
+}
---
+++ Advance/LINQ/Bai03/Bai03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bai03
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			LinQExample.queryXMLFile("films.xml");
+		}
+	}
+}
