cc0a0e64ceefb4318b43a779a25ea289de6a6e5d
HauXun <hautrieu65@gmail.com>
2021-05-25 17:53:58 +0700

    Init Commit 🚀🚀🚀

---
+++ Advance/LINQ/Bai01/Bai01/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai01
+{
+	class Point
+	{
+		public double X { get; set; }
+		public double Y { get; set; }
+
+		public Point(double x, double y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public void ShowInfo()
+		{
+			Console.WriteLine($"x = {X}, y = {Y}");
+		}
+	}
+}
---
+++ Advance/LINQ/Bai01/Bai01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using static System.Console;
+
+namespace Bai01
+{
+	class Program
+	{
+		static IEnumerable<string> ReadFrom(string fileName)
+		{
+			var reader = File.OpenText(fileName);
+			WriteLine($"Type of reader = {reader.GetType()}");
+			string eachLine;
+			while ((eachLine = reader.ReadLine()) != null)
+			{
+				yield return eachLine;
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			var lines = ReadFrom("textFile.txt");
+			var points = new List<Point>();
+
+			foreach (var line in lines)
+			{
+				//WriteLine(line);
+				var numbers = line.Split(new[] { ", " }, StringSplitOptions.None);
+				var x = Convert.ToDouble(numbers[0]);
+				var y = Convert.ToDouble(numbers[1]);
+
+				//WriteLine($"x = {x}, y = {y}");
+
+				var newPoint = new Point(x, y);
+				points.Add(newPoint);
+			}
+			WriteLine("List of points");
+			foreach (var point in points)
+			{
+				point.ShowInfo();
+			}
+		}
+	}
+}
