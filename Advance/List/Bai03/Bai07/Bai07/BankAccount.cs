fa6152f5aea1663520a821c3270044d845502c6c
HauXun <hautrieu65@gmail.com>
2021-05-25 16:01:19 +0700

    Additonal Ex

---
+++ Advance/List/Bai03/Bai07/Bai07/BankAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai07
+{
+	class BankAccount
+	{
+		public long AccountNumber { get; }  // ReadOnly
+		public string Owner { get; set; }   // Can Read, Write value
+		private List<Transaction> transactions = new List<Transaction>();
+		public decimal Balance    // Can only read
+		{
+			get
+			{
+				decimal balance = 0;
+				foreach (var item in transactions)
+				{
+					balance += item.Amount;
+				}
+				return balance;
+			}
+		}
+
+		// Gửi tiền
+		public void Deposit(decimal amount, DateTime date, string note)
+		{
+			// Money sent to bank account
+			var deposit = new Transaction(amount, date, note);
+			transactions.Add(deposit);
+		}
+
+		public void Withdraw(decimal amount, DateTime date, string note)
+		{
+			// Withdraw money to buy something
+			var withdrawal = new Transaction(-amount, date, note);
+			transactions.Add(withdrawal);
+		}
+
+		private long GeneratorAccountNumbers() => ((DateTimeOffset)DateTime.Now).ToUnixTimeMilliseconds();
+		public BankAccount(string owner, decimal initialBalance)
+		{
+			Owner = owner;
+			AccountNumber = GeneratorAccountNumbers();
+			//Balance = initialBalance;
+			Console.WriteLine("Object initialized with \n" +
+				$"Account number = {AccountNumber}\n" +
+				$"Owner = {Owner}\n" +
+				$"Balance = {Balance}\n");
+		}
+	}
+}
---
+++ Advance/List/Bai03/Bai07/Bai07/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace Bai07
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var account = new BankAccount("Tran Dan", 1200);
+			WriteLine("Account has been created");
+			account.Deposit(300, DateTime.Now, "Receive this month's salary");
+			account.Withdraw(100, DateTime.Now, "Bye a keyboard");
+			account.Withdraw(120, DateTime.Now, "Buy a mouse");
+			account.Withdraw(1500, DateTime.Now, "Buy a laptop");
+			WriteLine("Finish transactions");
+		}
+	}
+}
