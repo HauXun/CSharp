2ef8aa17ac03d88d8e01dbe11674e60cfcc1c759
HauXun <hautrieu65@gmail.com>
2021-05-27 10:05:29 +0700

    Additonal Ex

---
+++ Advance/ThuNghiemTrucTuyen/Course 01/SortedList/SortedList/PersonComparer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+namespace SortedLists
+{
+	class PersonComparer : IComparer
+	{
+		public int Compare(object x, object y)
+		{
+			// Ép kiểu 2 object truyền vào về Person
+			Person p1 = x as Person;
+			Person p2 = y as Person;
+
+			/*
+			Vì có thể 2 object truyền vào không phải Person khi đó ta không thể so sánh được.
+			Trường hợp này tốt nhất là chúng ta nên ném ra lỗi để lập trình viên sửa chữa.
+			 */
+			if (p1 == null || p2 == null)
+			{
+				throw new InvalidOperationException();
+			}
+			else
+			{
+				/*
+				Khi dữ liệu đã ok thì ta thực hiện so sánh và trả về các giá trị 1 0 -1 tương ứng
+				lớn hơn, bằng, bé hơn
+				 */
+				if (p1.Age < p2.Age)
+				{
+					return 1;
+				}
+				else if (p1.Age > p2.Age)
+				{
+					return -1;
+				}
+				else
+				{
+					return 0;
+				}
+			}
+		}
+		}
+	}
---
+++ Advance/ThuNghiemTrucTuyen/Course 01/SortedList/SortedList/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using static System.Console;
+
+namespace SortedLists
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			#region MyRegion
+
+			SortedList MySL = new SortedList();     // Khởi tạo 1 SortedList rỗng
+
+			SortedList MySL2 = new SortedList(5);   // Khởi tạo 1 SortedList và chỉ định Capacity ban đầu là 5
+
+			/*
+			Khởi tạo 1 SortedList có kích thước bằng với MySL2
+			Sao chép toàn bộ phần tử trong MySL2 vào MySL3
+			 */
+			SortedList MySL3 = new SortedList(MySL2);
+
+			/*
+			Mình định nghĩa 1 lớp PersonCompare có thực thi 1 interface IComparer
+			Sau đó ovveride lại phương thức Compare
+			Sử dụng lớp trên để truyền vào Constructor của SortedList
+			 */
+			SortedList MySL4 = new SortedList(new PersonComparer());
+
+			/*
+			Tạo 1 SortedList mới và sao chép các phần tử từ MySL3 đồng thời sắp xếp các phần tử lại
+			theo cách sắp xếp được định nghĩa trong lớp PersonComparer
+			 */
+			SortedList MySL5 = new SortedList(MySL3, new PersonComparer());
+
+			#endregion
+
+			#region Ví dụ sử dụng SortedList
+			// Tạo 1 SortedList và truyền vào cách sắp xếp các Key trong SortedList này.
+			SortedList MySl6 = new SortedList(new PersonComparer());
+			MySl6.Add(new Person("Tran Dan", 65), 66);
+			MySl6.Add(new Person("Hai Zu", 59), 58);
+
+			foreach (DictionaryEntry item in MySl6)
+			{
+				WriteLine(" Key: " + item.Key + "\tValue: " + item.Value);
+			}
+
+			#endregion
+		}
+	}
+}
