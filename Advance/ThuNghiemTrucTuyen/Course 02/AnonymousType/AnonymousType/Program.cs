f0112d22cb4cc7c1b3b1f356f44b842dd12c0908
HauXun <hautrieu65@gmail.com>
2021-05-28 07:52:15 +0700

    Additonal Ex

---
+++ Advance/ThuNghiemTrucTuyen/Course 02/AnonymousType/AnonymousType/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace AnonymousType
+{
+	class Program
+	{
+
+		// Anonymous - Kiểu dữ liệu vô danh
+		// Object - Thuộc tính - Chỉ đọc
+		// new {thuoctinh = giatri, thuoctinh2 = giatri2}
+
+		static void Main(string[] args)
+		{
+			var sanpham = new {
+				Ten = "Iphone 8",
+				Gia = 1000,
+				NamSx = 2018
+			};
+
+			WriteLine(sanpham.Ten);
+			WriteLine(sanpham.Gia);
+
+			List<Sinhvien> sinhvien = new List<Sinhvien>() { 
+				new Sinhvien() {HoTen = "Nam", NamSinh = 2000, NoiSinh = "Binh Duong"},
+				new Sinhvien() {HoTen = "Dan", NamSinh = 2002, NoiSinh = "Nam Dinh"},
+				new Sinhvien() {HoTen = "Long", NamSinh = 2001, NoiSinh = "Vinh Phuc"},
+				new Sinhvien() {HoTen = "Minh", NamSinh = 2000, NoiSinh = "Kien Giang"}
+			};
+
+			var ketqua = from sv in sinhvien
+						 where sv.NamSinh < 2001
+						 select new {
+							 Ten = sv.HoTen,
+							 NS = sv.NoiSinh
+						 };
+
+			foreach (var sv in ketqua)
+			{
+				WriteLine(sv.Ten + " - " + sv.NS);
+			}
+		}
+	}
+}
---
+++ Advance/ThuNghiemTrucTuyen/Course 02/AnonymousType/AnonymousType/Sinhvien.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AnonymousType
+{
+	class Sinhvien
+	{
+		public string HoTen { get; set; }
+		public int NamSinh { get; set; }
+		public string NoiSinh { get; set; }
+	}
+}
