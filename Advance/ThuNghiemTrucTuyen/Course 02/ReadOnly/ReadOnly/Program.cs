42790e5e5632413c5629546d817c8e54e3423fb4
HauXun <hautrieu65@gmail.com>
2021-05-28 12:23:54 +0700

    Additional Ex

---
+++ Advance/ThuNghiemTrucTuyen/Course 02/ReadOnly/ReadOnly/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ReadOnly
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Student student = new Student("Tran Dan");
+			Console.WriteLine(student.name);
+		}
+	}
+}
---
+++ Advance/ThuNghiemTrucTuyen/Course 02/ReadOnly/ReadOnly/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ReadOnly
+{
+	class Student
+	{
+		public readonly string name;    // Chỉ đọc
+
+		// Có thể được gán thông qua Constructor
+		public Student(string name)
+		{
+			this.name = name;
+		}
+	}
+}
