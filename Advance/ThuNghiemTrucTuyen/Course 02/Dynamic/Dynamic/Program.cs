6f66a9676a1be6989e6971b4cf42cade769a6d45
HauXun <hautrieu65@gmail.com>
2021-05-28 08:00:09 +0700

    Additonal Ex

---
+++ Advance/ThuNghiemTrucTuyen/Course 02/Dynamic/Dynamic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Dynamic
+{
+	class Program
+	{
+		// Dynamic - Kiểu dữ liệu động
+
+		static void PrintInfo(dynamic obj)
+		{
+			obj.Name = "Tran Dan";
+			obj.Hello();
+		}
+
+		static void Main(string[] args)
+		{
+			dynamic tenbien;
+
+			tenbien = new { };
+
+			tenbien.AbcXyz = "Son Tung";
+			tenbien.CoSungDayNe();
+
+			Student student = new Student();
+			PrintInfo(student);
+		}
+	}
+}
---
+++ Advance/ThuNghiemTrucTuyen/Course 02/Dynamic/Dynamic/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Dynamic
+{
+	class Student
+	{
+		public string Name { get; set; }
+		public void Hello() => Console.WriteLine(Name);
+	}
+}
