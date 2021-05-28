a1f4a6aca013e44b85c3e0c49dd83f789de110cb
HauXun <hautrieu65@gmail.com>
2021-05-28 12:16:30 +0700

    Additional Ex

---
+++ Advance/ThuNghiemTrucTuyen/Course 02/Static/Static/CountNumber.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Static
+{
+	class CountNumber
+	{
+		public static int number = 0;
+		public static void Info() => Console.WriteLine("So lan truy cap: " + number);
+
+		public void Count() => CountNumber.number++;
+	}
+}
---
+++ Advance/ThuNghiemTrucTuyen/Course 02/Static/Static/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace Static
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CountNumber.Info();
+			WriteLine($"{CountNumber.number.GetType()}: {CountNumber.number}");
+
+			CountNumber number1 = new CountNumber();
+			CountNumber number2 = new CountNumber();
+
+			number1.Count();
+			number2.Count();
+			CountNumber.Info();
+		}
+	}
+}
