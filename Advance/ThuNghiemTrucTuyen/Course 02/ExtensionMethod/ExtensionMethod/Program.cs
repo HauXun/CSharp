8dbfc9920f9bd7c387410c3d9802ed3f602927b3
HauXun <hautrieu65@gmail.com>
2021-05-28 11:45:41 +0700

    Additional Ex

---
+++ Advance/ThuNghiemTrucTuyen/Course 02/ExtensionMethod/ExtensionMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using MyLib;
+using static System.Console;
+
+namespace ExtensionMethod
+{
+	// Extension Method
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] a = { 1, 7, 4, 9, 3 };
+			int max = a.Max();
+			WriteLine(max);
+
+			string s = "Xin chao Tran Dan nha";
+			"Xin".Print(ConsoleColor.Red);
+			"Chao".Print(ConsoleColor.Yellow);
+			"Tran".Print(ConsoleColor.Magenta);
+			"Dan".Print(ConsoleColor.Gray);
+			"Nha".Print(ConsoleColor.Cyan);
+
+			ResetColor();
+
+			double b = 2.5;
+			WriteLine(b.BinhPhuong());
+			WriteLine(b.CanBacHai());
+			WriteLine(b.Sin());
+			WriteLine(b.Cos());
+		}
+
+		
+	}
+}
---
+++ Advance/ThuNghiemTrucTuyen/Course 02/ExtensionMethod/ExtensionMethod/StaticClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace ExtensionMethod
+{
+	// Lớp tĩnh static ~
+	static class StaticClass
+	{
+		public static void Print(this string s, ConsoleColor color)
+		{
+			ForegroundColor = color;
+			WriteLine(s);
+		}
+	}
+}
