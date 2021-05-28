2b4138de41144c357ffe072cd13be75556a2edc9
HauXun <hautrieu65@gmail.com>
2021-05-28 13:08:21 +0700

    Additional Ex

---
+++ Advance/ThuNghiemTrucTuyen/Course 02/Indexer/Indexer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Indexer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Vector vector = new Vector();
+			vector[0] = 5;
+			vector[1] = 6;
+			vector.Info();
+
+			//Console.WriteLine(vector[3]);
+
+			vector["toadox"] = 10;
+			vector["toadoy"] = 20;
+			vector.Info();
+		}
+	}
+}
---
+++ Advance/ThuNghiemTrucTuyen/Course 02/Indexer/Indexer/Vector.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using static System.Console;
+
+namespace Indexer
+{
+	class Vector
+	{
+		private double x;
+		private double y;
+
+		public Vector()
+		{
+
+		}
+
+		public Vector(double x, double y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public void Info() => WriteLine($"x = {x}, y = {y}");
+
+		public double this[int i]
+		{
+			get
+			{
+				switch (i)
+				{
+					case 0:
+						return x;
+					case 1:
+						return y;
+					default:
+						throw new Exception("Sai chi so");
+				}
+			}
+			set
+			{
+				switch (i)
+				{
+					case 0:
+						x = value;
+						break;
+					case 1:
+						y = value;
+						break;
+					default:
+						throw new Exception("Sai chi so");
+				}
+			}
+		}
+
+		public double this[string s]
+		{
+			get
+			{
+				switch (s)
+				{
+					case "toadox":
+						return x;
+					case "toadoy":
+						return y;
+					default:
+						throw new Exception("Sai chi so");
+				}
+			}
+			set
+			{
+				switch (s)
+				{
+					case "toadox":
+						x = value;
+						break;
+					case "toadoy":
+						y = value;
+						break;
+					default:
+						throw new Exception("Sai chi so");
+				}
+			}
+		}
+	}
+}
