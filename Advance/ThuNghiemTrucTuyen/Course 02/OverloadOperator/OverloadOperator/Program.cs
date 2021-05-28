f56db2ac429c3df865224bec92b0d6341e4c37ce
HauXun <hautrieu65@gmail.com>
2021-05-28 12:51:31 +0700

    Additional Ex

---
+++ Advance/ThuNghiemTrucTuyen/Course 02/OverloadOperator/OverloadOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OverloadOperator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Vector vector1 = new Vector(2, 3);
+			Vector vector2 = new Vector(1, 1);
+
+			// ((x1, y1), (x2, y2)) => (x1 + x2, y1 + y2)
+			var vector3 = vector1 + vector2;
+			vector1.Info();
+			vector2.Info();
+			vector3.Info();
+
+			var vector4 = vector3 + 10;
+			vector4.Info();
+		}
+	}
+}
---
+++ Advance/ThuNghiemTrucTuyen/Course 02/OverloadOperator/OverloadOperator/Vector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace OverloadOperator
+{
+	class Vector
+	{
+		private double x;
+		private double y;
+
+		public Vector(double x, double y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public void Info() => WriteLine($"x = {x}, y = {y}");
+
+		// vector1 + vector2 => vectorResult;
+		public static Vector operator +(Vector v1, Vector v2)
+		{
+			return new Vector(v1.x + v2.x, v1.y + v2.y);
+		}
+
+		public static Vector operator +(Vector v1, double v2)
+		{
+			return new Vector(v1.x + v2, v1.y + v2);
+		}
+	}
+}
