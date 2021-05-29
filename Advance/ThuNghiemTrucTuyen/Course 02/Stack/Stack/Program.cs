37e663b92b0e3675c1bfc42db9b2569f8165ad72
HauXun <hautrieu65@gmail.com>
2021-05-29 07:42:59 +0700

    Additional Ex

---
+++ Advance/ThuNghiemTrucTuyen/Course 02/Stack/Stack/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Stacks
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Stack<string> hanghoa = new Stack<string>();
+
+			hanghoa.Push("Mat hang 1");
+			hanghoa.Push("Mat hang 2");
+			hanghoa.Push("Mat hang 3");
+
+			var mathang = hanghoa.Pop();
+			WriteLine($"Boc do: {mathang}");
+
+			mathang = hanghoa.Pop();
+			WriteLine($"Boc do: {mathang}");
+
+			mathang = hanghoa.Pop();
+			WriteLine($"Boc do: {mathang}");
+		}
+	}
+}
