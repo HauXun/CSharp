ce50be100bbf1bc314ea3a66cccf898df9dd4cfd
HauXun <hautrieu65@gmail.com>
2021-05-29 08:22:59 +0700

    Additional Ex

---
+++ Advance/ThuNghiemTrucTuyen/Course 01/Event/DelegateEvent/DelegateEvent/HocSinh.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DelegateEvent
+{
+	class HocSinh
+	{
+		public event UpdateNameHandler NameChanged;
+
+		private string name;
+		public string Name
+		{
+			get => name;
+			set
+			{
+				name = value;
+				if (NameChanged != null)
+					NameChanged(name);
+			}
+		}
+	}
+}
---
+++ Advance/ThuNghiemTrucTuyen/Course 01/Event/DelegateEvent/DelegateEvent/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using static System.Console;
+
+namespace DelegateEvent
+{
+	delegate void UpdateNameHandler(string name);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			OutputEncoding = Encoding.Unicode;
+
+			HocSinh hocSinh = new HocSinh();
+			hocSinh.NameChanged += HocSinh_NameChanged;
+
+			hocSinh.Name = "Trần Dần";
+			WriteLine("Tên từ Class: " + hocSinh.Name);
+
+			hocSinh.Name = "Sơn Tùng";
+			WriteLine("Tên từ Class: " + hocSinh.Name);
+		}
+
+		private static void HocSinh_NameChanged(string name)
+		{
+			WriteLine("Tên mới: " + name);
+		}
+	}
+}
