f3933d7b2245011bf4d71a97502a2909b4c2d3ed
HauXun <hautrieu65@gmail.com>
2021-05-29 07:41:49 +0700

    Additional Ex

---
+++ Advance/ThuNghiemTrucTuyen/Course 02/Dictionary/Dictionary/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Dictionarys
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Khởi tạo với 2 phần tử
+			Dictionary<string, int> sodem = new Dictionary<string, int>()
+			{
+				["one"] = 1,
+				["two"] = 2
+			};
+
+			// Thêm hoặc cập nhập
+			sodem["three"] = 3;
+			sodem.Add("four", 4);
+
+			var keys = sodem.Keys;
+			foreach (var k in keys)
+			{
+				var value = sodem[k];
+				WriteLine($"{k} = {value}");
+			}
+
+			WriteLine();
+
+			foreach (KeyValuePair<string, int> item in sodem)
+			{
+				WriteLine($"{item.Key} -> {item.Value}");
+			}
+		}
+	}
+}
