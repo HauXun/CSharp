aa04bb46c6660e594a013adef6651d638f86e1dc
HauXun <hautrieu65@gmail.com>
2021-05-29 09:21:31 +0700

    Additional Ex

---
+++ Advance/ThuNghiemTrucTuyen/Course 01/Event/StandardEvent/StandardEvent/HocSinh.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StandardEvent
+{
+	class HocSinh
+	{
+		private string name;
+
+		public string Name 
+		{ 
+			get => name;
+			set
+			{
+				name = value;
+				OnNameChange(value);
+			}
+		}
+
+		private event EventHandler<NameChangedArgs> _NameChanged;
+		public event EventHandler<NameChangedArgs> NameChanged
+		{
+			add
+			{
+				_NameChanged += value;
+			}
+			remove
+			{
+				_NameChanged -= value;
+			}
+		}
+
+		private void OnNameChange(string name)
+		{
+			if (_NameChanged != null)
+			{
+				_NameChanged(this, new NameChangedArgs(name));
+			}	
+		}
+	}
+}
---
+++ Advance/ThuNghiemTrucTuyen/Course 01/Event/StandardEvent/StandardEvent/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using static System.Console;
+
+namespace StandardEvent
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			OutputEncoding = Encoding.Unicode;
+
+			HocSinh hocSinh = new HocSinh();
+			hocSinh.NameChanged += HocSinh_NameChanged;
+
+			hocSinh.Name = "Thay đổi lần 1.";
+			hocSinh.Name = "Thay đổi lần 2.";
+			hocSinh.Name = "Thay đổi lần 3.";
+		}
+
+		private static void HocSinh_NameChanged(object sender, NameChangedArgs e)
+		{
+			WriteLine("Tên có thay đổi là " + e.Name);
+		}
+	}
+}
