1cdbad7a74773aa0105ebb3ee7fcd3794f0c0ea6
HauXun <hautrieu65@gmail.com>
2021-05-29 07:42:51 +0700

    Additional Ex

---
+++ Advance/ThuNghiemTrucTuyen/Course 02/Queue/Queue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Queues
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Queue<string> hoso = new Queue<string>();
+
+			hoso.Enqueue("Ho so 1");
+			hoso.Enqueue("Ho so 2");
+			hoso.Enqueue("Ho so 3");
+
+			foreach (var item in hoso)
+			{
+				WriteLine(item);
+			}
+
+			var hs = hoso.Dequeue();
+			WriteLine($"Xu ly ho so: {hs} - {hoso.Count}");
+
+			hs = hoso.Dequeue();
+			WriteLine($"Xu ly ho so: {hs} - {hoso.Count}");
+
+			hs = hoso.Dequeue();
+			WriteLine($"Xu ly ho so: {hs} - {hoso.Count}");
+
+		}
+	}
+}
