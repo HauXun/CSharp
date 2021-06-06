a5d1423afb1e5a7caf25cf1905d0344621a12960
Carl <hautrieu65@gmail.com>
2021-06-06 15:06:20 -0700

    Additional Ex

---
+++ Advance/ThuNghiemTrucTuyen/Course 01/MultiThreading/MultiThreading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+
+namespace MultiThreading
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				var valueTemp = i;
+				Thread thread1 = new Thread(() =>
+				{
+					DemoThread("Thread " + valueTemp);
+				});
+				thread1.IsBackground = true;
+				thread1.Start();
+			}
+		}
+
+		static void DemoThread(string threadIndex)
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				Thread.Sleep(TimeSpan.FromSeconds(1));
+				WriteLine(threadIndex + " - " + i);
+			}
+		}
+	}
+}
